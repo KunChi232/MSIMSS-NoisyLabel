87d91e8a98045e5dd08798668e5dd163411169c6
KunChi232 <kuo23201199@gmail.com>
2021-02-02 04:24:02 +0000

    Add CrossEntorpy loss

---
+++ utils/loss.py
@@ -0,0 +1,8 @@
+class CrossEntropyLoss(nn.Module):
+    def __init__(self):
+        super().__init__()
+    @property
+    def __name__(self):
+        return "CELoss"
+    def forward(self, y_pred, y_gt):
+         return nn.CrossEntropyLoss()(y_pred, y_gt) 
